5415bfddfcabbea242ba5adf8ab56e21a5283550
AmadyaChz <ceci.dresser96@gmail.com>
2018-08-28 20:44:41 -0500

    fixed bugs in jQuery

--- assets/javascript/effects.js
+++ assets/javascript/effects.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
-var $hoverSound = $("#hover");
-
-    $(".character > .image").on('mouseenter', function() {
-        $hoverSound.get(0).play();
+    var $hoverSound = $("#hoverSound");
+    
+        $(".character > .image").on('mouseenter', function() {
+            $hoverSound.get(0).play();
+        });
+    
+        $(".character > .image").on('mouseout', function() {
+            $hoverSound.get(0).pause();
+            $hoverSound.get(0).currentTime = 0;
+        });
+    
     });
-
-    $(".character > .image").on('mouseout', function() {
-        $hoverSound.get(0).pause();
-        $hoverSound.get(0).currentTime = 0;
-    });
-
-});
